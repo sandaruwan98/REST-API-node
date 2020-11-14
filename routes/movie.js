7befe2e00d9c1ad172a69b861a6dc0526ff512cc
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-11-14 18:43:45 +0530

    search movie by title

--- routes/movie.js
+++ routes/movie.js
@@ -4,9 +4,21 @@ const Route = express.Router()
 
 const Movie = require('./../models/Movie')
 
+// get all movies with a limit
 Route.get('/all/:count', async (req,res) => {
     try {
         const movies = await Movie.find().limit( parseInt(req.params.count))
+        res.json(movies)
+        
+    } catch (error) {
+        res.json({"msg": error})
+    }
+})
+
+// search movie by title
+Route.get('/:title', async (req,res) => {
+    try {
+        const movies = await Movie.find({title: new RegExp(req.params.title)})
         res.json(movies)
         
     } catch (error) {
